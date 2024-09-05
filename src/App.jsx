f36a8defd6e85e8a643e1d8a1c5ddc14f2e65286
rashmi-punia <rashmipunia909@gmail.com>
2024-09-05 14:28:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ImageWidget from "./ImageWidget";
 const App = () => {
   return (
     <div className="md:flex-row flex flex-col gap-10 pl-[30px] px-[40px] py-[90px] bg-gradient-to-b from-mainTGrad to-mainBGrad text-white min-h-screen">
-      <div className="bg-ld1 bg-opacity-80 rounded-[27px] border border-blue-200 w-full md:flex-1"></div>
+      <div className="bg-ld1 bg-opacity-80 rounded-[27px] border border-blue-200 w-full sm:h-[100vh] h-[50vh] md:flex-1"></div>
       <div className="flex flex-col items-center gap-6">
         <Tabs />
         <span className="bottomSpan w-[50vw] lg:w-[612px] h-1.5" />
